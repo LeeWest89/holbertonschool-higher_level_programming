7165a655272e80dc0303dfa38c71a3bf4c809fe7
LeeWest89 <6683@holbertonstudents.com>
2023-09-05 12:26:44 -0700

    finally finding some good resources

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,13 @@
-#include
+#!/usr/bin/python3
+
+def uppercase(str):
+
+    s = ""
+
+    for char in str:
+        if 'a' <= char <= 'z':
+            upper = chr(ord(char) - ord('a') + ord('A'))
+            s += upper
+        else:
+            s += char
+    print(s)
