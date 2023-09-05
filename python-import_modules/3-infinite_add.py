f6b0173daffc77c598f1d3070d50923c1b82a152
LeeWest89 <6683@holbertonstudents.com>
2023-09-05 16:09:50 -0700

    i think it will add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
-#include
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    fs = 0
+    for i in range(len(sys.argv) - 1):
+        fs += int(sys.argv[i +1])
+    print ("{}".format(fs))
--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,14 @@
-#include
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv) - 1
+
+    if args == 0:
+        print("0 arguments.".format(args))
+    elif args == 1:
+        print("1 argument:".format(args))
+    else:
+        print("{} arguments:".format(args))
+    for i in range(args):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
