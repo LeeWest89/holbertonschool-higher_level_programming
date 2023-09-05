ae39002c7d3bef0d393b1bce3342791bcc301ba5
LeeWest89 <6683@holbertonstudents.com>
2023-09-05 12:07:54 -0700

    lets see

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,8 @@
-#include
+#!/usr/bin/python3
+
+for n1 in range(10):
+    for n2 in range(n1 + 1, 10):
+        if n1 < 8:
+            print("{:d}{:d}, ".format(n1, n2), end="")
+        else:
+            print("{:d}{:d}".format(n1, n2))
