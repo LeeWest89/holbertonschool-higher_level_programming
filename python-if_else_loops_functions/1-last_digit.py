0e138530b5c39ed5a2b2472519eedbcb51f65796
LeeWest89 <6683@holbertonstudents.com>
2023-09-05 11:00:42 -0700

    fixed trailing white space

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -10,10 +10,8 @@ if number < 0:
 if number >= 0:   
     if ld > 5:
         print(f'Last digit of {number} is {ld} and is greater than 5')
-
     elif ld == 0:
         print(f'Last digit of {number} is {ld} and is 0')
-
     else:
         print(f'Last digit of {number} is {ld} and is less than 6 and not 0')
 
