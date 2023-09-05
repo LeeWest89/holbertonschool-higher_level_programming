b8132f787325c8629a7fe9af4e4b20619a2a5c79
LeeWest89 <6683@holbertonstudents.com>
2023-09-05 01:45:51 -0700

    one more time i hope

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 import this
+
 def print_zen_of_python():
-    """
-    This function prints "The Zen of Python" by Tim Peters, followed by a new line.
-    The script is maximum 98 characters long.
-    """
     print(this.s)
