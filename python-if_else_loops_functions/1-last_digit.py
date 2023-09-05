6688c9dbd969e98d59b6003247ed0c6c9b46d785
LeeWest89 <6683@holbertonstudents.com>
2023-09-05 10:42:39 -0700

    create variable sign

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,6 +4,9 @@ number = random.randint(-10000, 10000)
 
 ld = abs(number) % 10
 
+if number < 0:
+    sign = '-'
+
 if ld > 5:
     print(f'Last digit of {number} is {ld} and is greater than 5')
 
@@ -11,4 +14,4 @@ elif ld == 0:
     print(f'Last digit of {number} is {ld} and is 0')
 
 else:
-    print(f'Last digit of {number} is {ld} and is less than 6 and not 0')
+    print(f'Last digit of {number} is {sign}{ld} and is less than 6 and not 0')
