9733491cb5e370c71dc567724e3915e0334b9bc9
LeeWest89 <6683@holbertonstudents.com>
2023-09-06 08:05:58 -0700

    trying something different for3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
+import sys
 if __name__ == "__main__":
-    import sys
 
-    fs = 0
+    result = 0
     for i in range(len(sys.argv) - 1):
-        fs += int(sys.argv[i +1])
-    print("{}".format(fs))
+        result += int(sys.argv[i +1])
+    print("{}".format(result))
