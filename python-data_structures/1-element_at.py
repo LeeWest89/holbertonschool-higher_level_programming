7a75b7db56afd2aef46f1b0e396f924f52ac7ef3
LeeWest89 <6683@holbertonstudents.com>
2023-09-09 15:08:56 -0700

    fixed tabs again

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-	if idx < 0:
-		return None
-	if idx > range:
-		return None
-	else:
-		return (my_list[idx:idx + 1])
+    if idx < 0:
+        return None
+    if idx > range:
+        return None
+    else:
+        return (my_list[idx:idx + 1])
