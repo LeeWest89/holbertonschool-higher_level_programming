69e99f6594ed552765cd490a1992f4531a6d1b68
LeeWest89 <6683@holbertonstudents.com>
2023-09-11 16:49:54 -0700

    final figured out how to get for and 9

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return (0)
+    roman_numerals = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    number = 0
+    prev = 0
+    for i in roman_string:
+        for key, value in roman_numerals.items():
+            if i == key:
+                if prev == 0 or prev >= value:
+                    number += value
+                elif prev < value:
+                    number += value - 2
+                prev = value
+    return (number)
