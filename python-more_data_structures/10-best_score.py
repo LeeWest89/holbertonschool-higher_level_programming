d561c1bb51b969cb3bf52a81e158315c0759747e
LeeWest89 <6683@holbertonstudents.com>
2023-09-11 14:39:57 -0700

    testing to see if this will work, hopefully its this simple

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-	if a_dictionary is None:
-            return (None)
-        bs = max(a_dictionary.values(), default=None)
-        for key, value in a_dictionary.items():
-            if value == bs:
-                return (key)
+    if a_dictionary is None:
+        return (None)
+    return (max(a_dictionary, key=a_dictionary.get))
