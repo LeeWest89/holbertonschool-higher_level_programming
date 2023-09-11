d28774ea2ac9f31c855538093a9372e6177ff628
LeeWest89 <6683@holbertonstudents.com>
2023-09-11 13:39:11 -0700

    similar to 8

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    m_dictionary = a_dictionary.copy()
+    for key, value in m_dictionary.items():
+        m_dictionary[key] = value * 2
+    return (m_dictionary)
