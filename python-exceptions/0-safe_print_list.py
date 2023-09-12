8bf538e98144ad2ddfcdcc8d38ca2fed8b75f5e2
LeeWest89 <6683@holbertonstudents.com>
2023-09-12 08:14:20 -0700

    frogot the true/false returns

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,6 +7,7 @@ def safe_print_list(my_list=[], x=0):
                 print("{}".format(my_list[idx]), end="")
                 idx += 1
         print()
+        return(idx)
     except IndexError:
-        pass
-    return (idx)
+        return(False)
+    return (True)
