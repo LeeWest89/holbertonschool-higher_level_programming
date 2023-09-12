191e8c2f0af6f0507af090c7c7526e23d35d609e
LeeWest89 <6683@holbertonstudents.com>
2023-09-12 12:44:07 -0700

    Overthought it orginally used try and except

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    result = 0
-    try:
-        result = a / b
-        print("{}".format, (result))
-        if result == 0:
-            return (None)
-    except:
+def raise_exception():
+    raise TypeError
