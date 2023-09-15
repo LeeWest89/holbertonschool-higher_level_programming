d3695698459a501ca5f02f4eee4fda86686a00d1
LeeWest89 <6683@holbertonstudents.com>
2023-09-15 16:13:36 -0700

    test

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -33,7 +33,7 @@ class Square:
     @position.setter
     def position(self, value):
         """Sets the position"""
-        if (value[0] < 0 or value[1] < 0 or
+        if (not all(i >= 0 for i in value) or
                 len(value) != 2 or
                 type(value[0]) is not int or type(value[1]) is not int or
                 type(value) is not tuple):
