65761b100f7e8e735449eda8a568a13efdc0aec0
LeeWest89 <6683@holbertonstudents.com>
2023-09-15 13:01:21 -0700

    found a good video that showed how to ser up getters setters and deleters

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""Defines the class Square."""
+
+
+class Square:
+    """Square with a size."""
+    def __init__(self, size=0):
+        """Intializes the Square."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Returns the size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Square is set"""
+        if type(value) is not int:
+            """Checks if vaule is an int"""
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            """Checks if value is positive"""
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of the square"""
+        return (self.__size ** 2)
