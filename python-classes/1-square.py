5d44db3643af3b5ee89dd0ca7d118e25aa0de7b6
LeeWest89 <6683@holbertonstudents.com>
2023-09-15 11:51:20 -0700

    adding to documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,7 +6,10 @@ class Square:
     """Square with a size."""
 
     def __init__(self, size):
-        """Initializes a square with a size."""
+        """Initializes a square with a size.
+
+        Args: size (int): Size of the square.
+        """
         self.__size = size
 
 
