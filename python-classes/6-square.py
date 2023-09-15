c9fa5302bfa055b289665ce2964d6748d7207c0b
LeeWest89 <6683@holbertonstudents.com>
2023-09-15 16:04:38 -0700

    working on that if statement

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -33,10 +33,10 @@ class Square:
     @position.setter
     def position(self, value):
         """Sets the position"""
-        if (value[0] < 0 or value[1] < 0 or
+        if (not all(i >= 0 for i in value) or
                 len(value) != 2 or
                 not all(isinstance(i, int) for i in value) or
-                not isinstance(value, tuple)):
+                type(value) is not tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
