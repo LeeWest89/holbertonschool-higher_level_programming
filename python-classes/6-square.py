328b7098f320ce0024612afa70a87c3fc0a5e392
LeeWest89 <6683@holbertonstudents.com>
2023-09-15 13:59:21 -0700

    it did not like l

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -55,7 +55,7 @@ class Square:
         for k in range(self.__position[1]):
             print()
         for i in range(self.__size):
-            for l in range(self.__position[0]):
+            for n in range(self.__position[0]):
                 print(" ", end="")
             for j in range(self.__size):
                 print("#", end="")
