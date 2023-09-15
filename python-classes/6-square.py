107bdf0bcd06626194f574827e9b3d842e8a1ae9
LeeWest89 <6683@holbertonstudents.com>
2023-09-15 14:15:21 -0700

    added one more TypeError

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -40,6 +40,9 @@ class Square:
         if type(value[0], value[1]) is not int:
             """Checks if value is tuple of two postive int"""
             raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value) is not tuple:
+            """Checks if value is tuple of two postive int"""
+            raise TypeError("position must be a tuple of 2 positive integers")
         elif len(value) != 2:
             """Check if position has two elements"""
             raise TypeError("position must be a tuple of 2 positive integers")
