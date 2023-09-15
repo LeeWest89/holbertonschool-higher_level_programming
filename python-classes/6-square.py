9d2332a5381a33e8d62dbd5baa2895a7ae8749d7
LeeWest89 <6683@holbertonstudents.com>
2023-09-15 14:28:21 -0700

    trying one more thing, adding an else statement on line 48

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,7 +45,8 @@ class Square:
         elif value[0] < 0 or value[1] < 0:
             """Checks if value isless than zero"""
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
         """Returns the area of the square"""
