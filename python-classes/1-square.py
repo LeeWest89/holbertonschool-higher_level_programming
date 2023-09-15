f1aca0a571fc717f058cf2256b4d53f4716476ef
LeeWest89 <6683@holbertonstudents.com>
2023-09-15 11:26:30 -0700

    i think this will work i hope the documentation is correct

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Defines the class Square"""
+
+
+class Square:
+    """Square with a size"""
+    def __init__(self, size):
+        """Starts a square with a size
+
+        Args:
+            size(int): The size of the square.
+        """
+        self.__size = size
+
+
