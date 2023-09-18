d098c8fac72e8b743df21e8bd47bada35851da20
LeeWest89 <6683@holbertonstudents.com>
2023-09-18 12:15:11 -0700

    lets seeif it works or if i have more work to do on the test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
+def add_integer(a, b=98):
+	"""Adds to Integers together"""
+	if not (isinstance(a, int) or isinstance(a, float)):
+		raise TypeError("a must be an integer")
+	if not (isinstance(b, int) or isinstance(b, float)):
+		raise TypeError("b must be an integer")
+	return int(a + b)
