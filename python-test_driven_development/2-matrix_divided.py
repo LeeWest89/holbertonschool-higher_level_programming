bc542306af75f36fb48b818fb1aebae5d0adc0f8
LeeWest89 <6683@holbertonstudents.com>
2023-09-18 15:39:57 -0700

    this was a lot of trial and error

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
+"""Divides all elements in a matrix as lon as they are a float or an integer"""
+
+
+def matrix_divided(matrix, div):
+    """Divides all elements in the matrix"""
+    nm = []
+    result = []
+    col = len(matrix[0])
+
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for row in matrix:
+        if len(row) != col:
+            raise TypeError("Each row of the matrix must have the same size")
+    for row in matrix:
+        for elem in row:
+            if type(elem) != int and float:
+                raise TypeError("matrix must be a matrix (list of lists) "
+                                "of integers/floats")
+    return [list(round(elem/div, 2) for elem in row) for row in matrix]
