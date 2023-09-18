2d3ebaa7bf9e4f6af29189005efeb2436ce50eab
LeeWest89 <6683@holbertonstudents.com>
2023-09-18 13:06:48 -0700

    fix pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,9 +2,9 @@
 """
 Function tht adds two numbers together and returns an integer
 """
+
+
 def add_integer(a, b=98):
-
-
     """Adds to Integers together"""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
