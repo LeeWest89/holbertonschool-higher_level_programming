09942ec27b2693a377904a6c0ff99305dca127b0
LeeWest89 <6683@holbertonstudents.com>
2023-09-18 12:53:33 -0700

    i hate my dyslexia

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+Function tht adds two numbers together and returns an integer
+"""
 def add_integer(a, b=98):
-	"""Adds to Integers together"""
-	if not (isinstance(a, int) or isinstance(a, float)):
-		raise TypeError("a must be an integer")
-	if not (isinstance(b, int) or isinstance(b, float)):
-		raise TypeError("b must be an integer")
-	return int(a + b)
+    """Adds to Integers together"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a + b)
