9121428157eef403b88adecb3e7247c984fab4b0
LeeWest89 <6683@holbertonstudents.com>
2023-09-19 12:23:10 -0700

    this one was that bad rectangle is now deleted

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -1,1 +1,58 @@
 #!/usr/bin/python3
+"""Defines the class Rectangle"""
+
+
+class Rectangle:
+    """Defines class Rectangle."""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        else:
+            return (2 * (self.__width + self.__height))
+
+    def __str__(self):
+        rec = ""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        for i in range(self.__height):
+            rec += "#" * self.__width
+            if i < self.__height - 1:
+                rec += "\n"
+        return (rec)
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
+    def __del__(self):
+        print("Bye rectangle...")
