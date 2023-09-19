d4f77c9248573978c81f13b228a3e9518ad73571
LeeWest89 <6683@holbertonstudents.com>
2023-09-18 19:39:25 -0700

    test

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
+"""Function that prints 2 new lines after . ? :"""
+
+
+def text_indentation(text):
+	"""Prints text and 2 new lines after . ? :"""
+	i = ""
+	if type(text) is not str:
+		raise TypeError("text must be a string")
+	for char in text:
+		i += char
+		if char in ['.', '?', ':']:
+			i += "\n\n"
+	print(i)
