393a9f3a40daa082e99cd12dcb0506d90c6e45d2
LeeWest89 <6683@holbertonstudents.com>
2023-09-23 15:27:07 -0700

    think i ran the doctest correctly

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Creating class that inherits from list"""
+
+
+class MyList(list):
+
+    def print_sorted(self):
+        """Prints a list in ascending order"""
+        sort_list = sorted(self)
+        print(sort_list)
