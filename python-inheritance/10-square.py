466db9d23bb1eddbd5cdb19576ef5e928aeb9477
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 11:03:38 -0700

    I think this is right if not i may have to bring in the area function

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""New class Square inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """Sets up the Square"""
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def __str__(self):
+        """Function to return info in desired format"""
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
+
+
