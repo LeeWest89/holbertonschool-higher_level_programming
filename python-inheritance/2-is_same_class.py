11c86c8adb94572705f4d3efdc2999acf891cb3f
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 09:02:45 -0700

    had to find how to get the exact class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Function that returns True if the object is the class ; otherwise False"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if the obj is the exact type of a_class"""
+    return type(obj) is a_class
