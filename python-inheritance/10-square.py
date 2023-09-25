9e2337755604432dba5c389525d0c08efde4d545
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 11:06:21 -0700

    brought in the area function

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -10,6 +10,10 @@ class Square(Rectangle):
         self.__size = size
         self.integer_validator("size", size)
 
+    def area(self):
+        """Using super to call method, returns the area of the square"""
+        return (self.__size * self.__size)
+
     def __str__(self):
         """Function to return info in desired format"""
         return ("[Rectangle] {}/{}".format(self.__size, self.__size))
