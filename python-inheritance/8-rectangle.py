d952075cd0a8b8872fd2d068a7d119b42b42e089
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 10:33:52 -0700

    testing 8 im not pulling a typeerror

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Class Rectangle inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Sets up class Rectangle"""
+    def __init__(self, width, height):
+        """Calls integer_validator forheight and width"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
