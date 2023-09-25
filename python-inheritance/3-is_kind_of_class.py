0479e0637884b41f1887300eb23b0e8227bdf1f9
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 09:08:56 -0700

    had this on 2. At least its not going to waste

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Function that checks if object is the same class as what it inherits"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if same type as class inherited from"""
+    if isinstance(obj, a_class):
+        return (True)
+    else:
+        return (False)
