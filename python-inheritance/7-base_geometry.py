8c651d18c217f66a3a3553dd1e0f7d803a48051f
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 10:07:47 -0700

    Everything looks good

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Creates class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Creates class BaseGeometry"""
+    def area(self):
+        """Public method that raises Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
