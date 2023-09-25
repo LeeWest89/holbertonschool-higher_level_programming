443059445d61584f27aee3ac17e331391a7eb42a
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 09:30:41 -0700

    it appears i set this up correctly, and i remembered to check pycodestyle

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Creates class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Creates class BaseGeometry"""
+    def area(self):
+        """Public method that raises Exception"""
+        raise Exception("area() is not implemented")
