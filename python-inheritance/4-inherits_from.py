add34a94f918dcc1459e8b051fb3fecf6c869a61
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 09:19:42 -0700

    this appears to be working

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Checks if the object is an instance of a class that inherited class"""
+
+
+def inherits_from(obj, a_class):
+    """Check if same type as class inherited from"""
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return (True)
+    else:
+        return (False)
