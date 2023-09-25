7ae79219ec96016278469e4b7722763d0f5f277d
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 11:21:02 -0700

    appears to be functing correctly

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""New class Square inherits from Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Sets up the Square"""
+    def __init__(self, size):
+        """Calls integer_validator for size"""
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """Using super to call method, returns the area of the square"""
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """Function to return info in desired format"""
+        return ("[Square] {}/{}".format(self.__size, self.__size))
