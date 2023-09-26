df338067df68dcf32d0f7651a2eef20ca0f5a946
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 22:14:53 -0700

    not googling what pascals triangle is sucks, finally saw the patter now i hope my code holds up

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """returns a list of integers representing the Pascal's triangle"""
+    if n <= 0:
+        return ([])
+
+    triangle = [[1]]
+    for i in range(1, n):
+        l = [1]
+        for j in range(1, i):
+            l.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        l.append(1)
+        triangle.append(l)
+    return (triangle)
