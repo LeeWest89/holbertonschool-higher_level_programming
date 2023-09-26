5b97258e5207182152b11ed9ed54caadbc70e083
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 21:00:05 -0700

    i'm getting a different output than the example

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -3,13 +3,20 @@
 
 
 class Student:
-	"""Defines student info"""
-	def __init__(self, first_name, last_name, age):
-		"""Instantiation with first_name, last_name and age"""
-		self.first_name = first_name
-		self.last_name = last_name
-		self.age = age
+    """Defines student info"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """ retrieves a dictionary representation of a Student instance"""
-		return (self.__dict__)
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            d = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    d[i] = getattr(self, i)
+            return (d)
