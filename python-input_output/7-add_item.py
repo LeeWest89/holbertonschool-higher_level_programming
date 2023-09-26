7dde0a0d92b61cc817abc4265bd9ae04f964f09f
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 19:38:45 -0700

    the import part was the worst part of this

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""""""
+
+
+import sys
+import json
+save_to_json = __import__("5-save_to_json_file").save_to_json_file
+load_from_json = __import__("6-load_from_json_file").load_from_json_file
+
+if ("add_items.json"):
+    jlist = load_from_json("add_items.json")
+else:
+    jlist = []
+jlist.extend(sys.argv[1:])
+save_to_json(jlist, "add_item.json")
+
