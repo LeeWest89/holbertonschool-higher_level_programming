87883cc8a5e5cff3e1a6a363bb31d80850623494
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 17:02:15 -0700

    loads not dumps

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Get an object from a JSON string"""
+import json
+
+
+def from_json_string(my_str):
+    """returns an object represented by a JSON string"""
+    return (json.loads(my_str))
