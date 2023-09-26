46e286547e2ff3cff606fdcff63ff95ae2141557
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 20:35:02 -0700

    had tabs

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,13 +3,13 @@
 
 
 class Student:
-	"""Defines student info"""
-	def __init__(self, first_name, last_name, age):
-		"""Instantiation with first_name, last_name and age"""
-		self.first_name = first_name
-		self.last_name = last_name
-		self.age = age
+    """Defines student info"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-	def to_json(self):
-		""" retrieves a dictionary representation of a Student instance"""
-		return (self.__dict__)
+    def to_json(self):
+        """ retrieves a dictionary representation of a Student instance"""
+        return (self.__dict__)
