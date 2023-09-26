3ec0fa9b39a652a1bbe5177ec4814ed8d42c4126
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 21:11:35 -0700

    testing to see how it does

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""defines class Student"""
+
+
+class Student:
+    """Defines student info"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            d = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    d[i] = getattr(self, i)
+            return (d)
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
