f475bce88f6e2cd4c402ee6483001df5bf18d47f
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 19:45:54 -0700

    test for missed requirements

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,7 +6,6 @@ import sys
 import json
 save_to_json = __import__("5-save_to_json_file").save_to_json_file
 load_from_json = __import__("6-load_from_json_file").load_from_json_file
-
 if ("add_items.json"):
     jlist = load_from_json("add_items.json")
 else:
