c2c5f2462df54077c1c4faf30a5a87ab6a8bc938
LeeWest89 <6683@holbertonstudents.com>
2023-09-25 19:54:38 -0700

    had if else originally but had to change to try and except to get it to work

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,9 +6,9 @@ import sys
 import json
 save_to_json = __import__("5-save_to_json_file").save_to_json_file
 load_from_json = __import__("6-load_from_json_file").load_from_json_file
-if ("add_item.json"):
-    jlist = load_from_json("add_item.json")
-else:
+try:
+        jlist = load_from_json("add_item.json")
+except FileNotFoundError:
     jlist = []
 jlist.extend(sys.argv[1:])
 save_to_json(jlist, "add_item.json")
