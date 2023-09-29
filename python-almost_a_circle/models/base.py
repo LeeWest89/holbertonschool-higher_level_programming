b07b06c7aa48862506338dfe0228e4d591d92b6f
LeeWest89 <6683@holbertonstudents.com>
2023-09-29 12:14:13 -0700

    1 done

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Base class"""
+
+
+class Base:
+    """Base classfor id"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """How to find id"""
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
