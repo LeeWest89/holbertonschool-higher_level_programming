6ca44f45e973e0fc48fac19f33ecd8205bb6271f
LeeWest89 <6683@holbertonstudents.com>
2023-10-01 10:40:01 -0700

    seems to be returning the dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -36,3 +36,10 @@ class Base:
             else:
                 list_d = [ob.to_dictionary() for ob in list_objs]
                 f.write(Base.to_json_string(list_d))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns the list of the JSON string representation json_string"""
+        if json_string is None or json_string == "[]":
+            return ([])
+        return (json.loads(json_string))
