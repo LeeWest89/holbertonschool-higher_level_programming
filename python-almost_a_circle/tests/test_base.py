fff82bf8b4f240eb6a45d9d33e109f6e7c61e163
LeeWest89 <6683@holbertonstudents.com>
2023-10-02 09:23:40 -0700

    More test for test base

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -11,8 +11,13 @@ class TestBase(unittest.TestCase):
 
     def test_expected_id(self):
         Base._Base__nb_objects = 0
-        ob = Base(id=5)
+        ob = Base(5)
         self.assertEqual(ob.id, 5)
+
+    def test_two_argument_id(self):
+        Base._Base__nb_objects = 0
+        with self.assertRaises(TypeError):
+            Base(1, 2)
 
     def test_no_id(self):
         Base._Base__nb_objects = 0
@@ -42,7 +47,62 @@ class TestBase(unittest.TestCase):
 
     def test_same_id(self):
         Base._Base__nb_objects = 0
-        ob1 = Base(id=1)
-        ob2 = Base(id=1)
+        ob1 = Base(1)
+        ob2 = Base(1)
         self.assertNotEqual(ob1, ob2)
         self.assertEqual(ob1.id, ob2.id)
+
+    def test_negative_id(self):
+        Base._Base__nb_objects = -10
+        ob1 = Base()
+        ob2 = Base()
+        self.assertEqual(ob1.id, -9)
+        self.assertEqual(ob2.id, -8)
+
+    def test_after_unique_id(self):
+        Base._Base__nb_objects = 0
+        ob1 = Base()
+        ob2 = Base(100)
+        ob3 = Base()
+        self.assertEqual(ob3.id, 2)
+
+    def test_float_id(self):
+        Base._Base__nb_objects = 0
+        ob1 = Base(0.5)
+        ob2 = Base()
+        ob3 = Base(1.5)
+        self.assertEqual(ob1.id, 0.5)
+        self.assertEqual(ob2.id, 1)
+        self.assertEqual(ob3.id, 1.5)
+
+    def test_str_id(self):
+        Base._Base__nb_objects = 0
+        ob1 = Base("hello")
+        ob2 = Base()
+        self.assertEqual(ob1.id, "hello")
+        self.assertEqual(ob2.id, 1)
+
+    def test_list_id(self):
+        Base._Base__nb_objects = 0
+        ob1 = Base([1, 2, 3])
+        ob2 = Base([])
+        ob3 = Base()
+        self.assertEqual(ob1.id, [1, 2, 3])
+        self.assertEqual(ob2.id, [])
+        self.assertEqual(ob3.id, 1)
+
+    def test_dict_id(self):
+        Base._Base__nb_objects = 0
+        ob1 = Base({"a": 1, "b": 2})
+        ob2 = Base({})
+        ob3 = Base()
+        self.assertEqual(ob1.id, {"a": 1, "b": 2})
+        self.assertEqual(ob2.id, {})
+        self.assertEqual(ob3.id, 1)
+
+    def test_bool_id(self):
+        Base._Base__nb_objects = 0
+        ob1 = Base(True)
+        ob2 = Base()
+        self.assertEqual(ob1.id, True)
+        self.assertEqual(ob2.id, 1)
