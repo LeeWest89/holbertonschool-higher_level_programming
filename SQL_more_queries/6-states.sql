503e3f42a610543eec6065623498ee23ec399daa
LeeWest89 <6683@holbertonstudents.com>
2023-10-21 12:55:48 -0700

    Creates database and table with ids that auto increment

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,1 +1,7 @@
-#!/usr/bin/python3
+-- Creates database and table with ids that auto increment
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	PRIMARY KEY (id),
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+);
