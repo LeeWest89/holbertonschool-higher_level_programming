faf4e0544a6b6aada12b443919f51cc129a6a892
LeeWest89 <6683@holbertonstudents.com>
2023-10-21 13:17:44 -0700

    Need to remember line length

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,1 +1,3 @@
-#!/usr/bin/python3
+-- Creates database and table, list all cities in California on database
+SELECT id, name FROM cities WHERE state_id = 
+(SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
