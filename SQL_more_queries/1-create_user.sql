c49a112786d0d4c557aa7060e6246c12e2a359a6
LeeWest89 <6683@holbertonstudents.com>
2023-10-21 12:02:31 -0700

    creates user, sets password, and grants privileges.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,1 +1,4 @@
-#!/usr/bin/python3
+-- Creates user, grant all privileges, and sets password.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
