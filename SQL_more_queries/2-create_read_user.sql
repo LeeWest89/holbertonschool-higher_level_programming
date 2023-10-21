58c3cadb0f9001cc1f3c481ad42da553f86a81e2
LeeWest89 <6683@holbertonstudents.com>
2023-10-21 12:19:51 -0700

    testing syntax

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 -- Creates database and user, grant select privileges, and sets password.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_Od_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
