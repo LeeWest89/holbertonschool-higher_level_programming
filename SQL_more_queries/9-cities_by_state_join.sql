a77a08621da916c789eec623615d827847436ddf
LeeWest89 <6683@holbertonstudents.com>
2023-10-21 13:29:19 -0700

    Had to learn about join seems to be working properly

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,3 @@
-#!/usr/bin/python3
+--  lists all cities contained in the database in ascending order based on cities.id
+SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa.cities
+JOIN hbtn_0d_usa.states ON cities.state_id = states.id ORDER BY cities.id ASC;
