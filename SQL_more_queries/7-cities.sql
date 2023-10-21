4317c4684a2100a0b625ab228756570f4d31f85f
LeeWest89 <6683@holbertonstudents.com>
2023-10-21 13:05:05 -0700

    2 tables working together

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+-- Creates database(hbtn_0d_usa) and table(states and cities) with ids that auto increment.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	PRIMARY KEY (id),
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id),
+	id INT NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL
+);
