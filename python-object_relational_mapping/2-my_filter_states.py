c7629e3b1fb5af530077fe35a124c5d08797561a
LeeWest89 <6683@holbertonstudents.com>
2023-10-22 16:21:32 -0700

    figured out that i have to use .format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = '{}' \
+                   ORDER BY id ASC".format(sys.argv[4]))
+    [print(state) for state in cursor.fetchall()]
+    cursor.close()
+    db.close()
