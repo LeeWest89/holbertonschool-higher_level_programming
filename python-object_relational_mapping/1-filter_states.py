658ff6748d7eb326f83e217986ab3491b3f46a19
LeeWest89 <6683@holbertonstudents.com>
2023-10-22 16:02:54 -0700

    simple modification to 0 to check first letter of state name

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""lists all states with a name starting with N (upper N) from the database"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    [print(state) for state in cursor.fetchall() if state[1][0] == "N"]
+    cursor.close()
+    db.close()
