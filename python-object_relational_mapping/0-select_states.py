51eb8edd9ebf6a3d3e74079e0f4e7e2890e49bde
LeeWest89 <6683@holbertonstudents.com>
2023-10-22 15:58:21 -0700

    forgot pycodestyle check

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,10 +6,10 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-	db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-					  password=sys.argv[2], database=sys.argv[3])
-	cursor = db.cursor()
-	cursor.execute("SELECT * FROM states ORDER BY id ASC")
-	[print(state) for state in cursor.fetchall()]
-	cursor.close()
-	db.close()
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    [print(state) for state in cursor.fetchall()]
+    cursor.close()
+    db.close()
