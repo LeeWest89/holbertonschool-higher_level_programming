8445acdd1f29c9aef8a815b6861cc869fdddb8a9
LeeWest89 <6683@holbertonstudents.com>
2023-10-23 08:40:37 -0700

    Took a bit to figure out but its a modification of 7 never had to check for a letter using in in python

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""lists all State objects that contain the letter a from the database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
+    session.close()
