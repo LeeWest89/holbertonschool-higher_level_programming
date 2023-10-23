6c32452664d61210987c2021998a3534dad98149
LeeWest89 <6683@holbertonstudents.com>
2023-10-23 07:50:26 -0700

    created table states

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Defines State, inherits from Base, and links to MySQL table states"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+	"""state for MySQL db"""
+	__tablename__ = "states"
+	id = Column(Integer, primary_key=True)
+	name = Column(String(128), nullable=False)
