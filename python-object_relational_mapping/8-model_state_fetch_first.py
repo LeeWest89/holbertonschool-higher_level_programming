6b0d44bd06be41c0bbd2f386612ee32af6032820
LeeWest89 <6683@holbertonstudents.com>
2023-10-23 08:32:53 -0700

    prints the first state and id or nothing, turns out  State in this form is not iterable so had to change approach

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""prints the first State object from the database hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
