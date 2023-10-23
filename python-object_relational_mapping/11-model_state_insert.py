bbaad13d56f1a4a29a773de223db724783f0fdc1
LeeWest89 <6683@holbertonstudents.com>
2023-10-23 16:54:29 -0700

    adds Louisiana had to figure out session.commit()

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""adds the State object “Louisiana” to the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    add_state = State(name="Louisiana")
+    session.add(add_state)
+    state = session.query(State).filter_by(name="Louisiana").first()
+    print("{}".format(state.id))
+    session.commit()
