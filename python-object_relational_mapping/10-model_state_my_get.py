ddb5858bcb18ca5d468fa2b5aa9376437bb3c347
LeeWest89 <6683@holbertonstudents.com>
2023-10-23 16:15:43 -0700

    figured out how to deal with my IndexError issue

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -13,12 +13,14 @@ if __name__ == "__main__":
                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                         pool_pre_ping=True)
     Base.metadata.create_all(eng)
-    s = sys.argv[4]
+    if len(sys.argv) != 5:
+        print("Not Found")
+        sys.exit(1)
     Session = sessionmaker(bind=eng)
     session = Session()
-    state = session.query(State).filter(State.name == s).first()
-    if state is None:
-        print("Not Found")
-    else:
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    if state:
         print("{}".format(state.id))
+    else:
+        print("Not Found")
     session.close()
