d7c3048ae5c5a151a4ac7d68a3335d59bb6eedca
LeeWest89 <6683@holbertonstudents.com>
2023-10-23 19:02:22 -0700

    can change state based on id

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""changes the name of a State object from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    session.commit()
