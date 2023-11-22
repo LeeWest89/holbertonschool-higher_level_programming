aa42cb9cb4492fa05f1033102153e5c86e908c75
LeeWest89 <6683@holbertonstudents.com>
2023-11-22 12:41:41 -0800

    finally figured out how to set the output

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,1 +1,8 @@
-#include
+#!/usr/bin/node
+if (process.argv[2] && process.argv[3]) {
+  console.log(`${process.argv[2]} is ${process.argv[3]}`);
+} else if (process.argv[2]) {
+  console.log(`${process.argv[2]} is ${process.argv[3]}`);
+} else {
+  console.log(`${process.argv[2]} is ${process.argv[3]}`);
+}
