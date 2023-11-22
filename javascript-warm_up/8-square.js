531e9176dcd4381a5c0425acf82e835042f06fda
LeeWest89 <6683@holbertonstudents.com>
2023-11-22 14:38:44 -0800

    this was almost exactly like 7 in my console.log i used repeat to form the X

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,9 @@
-#include
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+if (num) {
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
+} else {
+  console.log('Missing size');
+}
