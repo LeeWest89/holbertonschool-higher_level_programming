544a092a4ab00a3356d3dadccaeddafbb27d757c
LeeWest89 <6683@holbertonstudents.com>
2023-11-22 16:09:26 -0800

    found a way to do this in some of my notes on sorting, had to figure out how to convert

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,21 @@
-#include
+#!/usr/bin/node
+const num = process.argv.slice(2);
+
+if (num.length === 0 || num.length === 1) {
+  console.log(0);
+} else {
+  const nums = [];
+  for (let i = 0; i < num.length; i++) {
+    nums.push(parseInt(num[i], 10));
+  }
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = 0; j < nums.length - i - 1; j++) {
+      if (nums[j] < nums[j + 1]) {
+        const temp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
+      }
+    }
+  }
+  console.log(nums[1]);
+}
