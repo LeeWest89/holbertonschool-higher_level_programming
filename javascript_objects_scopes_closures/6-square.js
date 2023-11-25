697a574f6b325ef494b8366f5bbca9b58546eff7
LeeWest89 <6683@holbertonstudents.com>
2023-11-25 14:55:30 -0800

    this is pretty straight forward if c is undefined you get a square of Xs othwise you get the specified charater

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,20 @@
-#include
+#!/usr/bin/node
+const Square = require('./5-square');
+
+class SquareC extends Square {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    if (this.width !== undefined && this.height !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = SquareC;
