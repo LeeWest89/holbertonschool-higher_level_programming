57ec003072fb288c1cd182f7c860075018f5a070
LeeWest89 <6683@holbertonstudents.com>
2023-11-30 11:12:50 -0800

    I'm now able to get the status code

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// reads and prints the contents of a file
+// write and prints the contents of a file
 const fs = require('fs');
 const fp = process.argv[2];
 const words = process.argv[3];
--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,12 @@
-#include
+#!/usr/bin/node
+// display the status code of a GET request.
+const req = require('request');
+const url = process.argv[2];
+
+req(url, (e, data) => {
+  if (e) {
+    console.error(e);
+  } else {
+    console.log(`code: ${data.statusCode}`);
+  }
+});
