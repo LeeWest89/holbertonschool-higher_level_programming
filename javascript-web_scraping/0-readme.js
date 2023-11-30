a12c00a58a2ff03ec4c6f938dd4e8a409264f2f1
LeeWest89 <6683@holbertonstudents.com>
2023-11-30 10:45:34 -0800

    intial commit, 0-readme.js is testing well.

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// reads and prints the contents of a file
+const fs = require('fs');
+const fp = process.argv[2];
+
+fs.readFile(fp, 'utf-8', (e, data) => {
+  if (data) {
+    console.log(data);
+  } else {
+    console.error(e);
+  }
+});
