064f6c7aa13a9cb63b272062c6d384efb89b5278
LeeWest89 <6683@holbertonstudents.com>
2023-11-30 11:24:44 -0800

    got the movie title, learned thow to set the url for the api and that i need the response

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,14 @@
-#include
+#!/usr/bin/node
+// prints the title of a Star Wars movie where the episode number matches a given integer.
+const req = require('request');
+const num = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${num}/`;
+
+req(url, (e, response, data) => {
+  if (e) {
+    console.error(e);
+  } else {
+    const movie = JSON.parse(data);
+    console.log(movie.title);
+  }
+});
