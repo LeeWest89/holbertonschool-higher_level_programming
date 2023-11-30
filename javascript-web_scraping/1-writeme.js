d6459a4cded6863f81c9393de71ca11c24cae50f
LeeWest89 <6683@holbertonstudents.com>
2023-11-30 11:02:19 -0800

    1 is testing well from the examples i think this should work.

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
-#include
+#!/usr/bin/node
+// reads and prints the contents of a file
+const fs = require('fs');
+const fp = process.argv[2];
+const words = process.argv[3];
+
+fs.writeFile(fp, words, 'utf-8', (e) => {
+  if (e) {
+    console.error(e);
+  }
+});
