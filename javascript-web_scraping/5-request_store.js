0ba9a4a71fd1c78f10b6daead887060d781b3660
LeeWest89 <6683@holbertonstudents.com>
2023-12-01 11:19:01 -0800

    correcting semistandard errors

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -7,13 +7,12 @@ const fp = process.argv[3];
 
 req(url, (e, response, data) => {
   if (e) {
-	console.error(e);
-	return;
+    console.error(e);
   } else {
     fs.writeFile(fp, data, 'utf-8', (e) => {
       if (e) {
         console.error(e);
 	  }
-	});
+    });
   }
 });
