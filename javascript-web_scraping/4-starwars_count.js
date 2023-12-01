1eff7dc51856ab0f92be1c6f5c2a71afeb1f95cc
LeeWest89 <6683@holbertonstudents.com>
2023-12-01 10:26:31 -0800

    I tried to over complicate 4 with a counter function, figured out you chan filter by movie.characters.includes.

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,14 @@
-#include
+#!/usr/bin/node
+// prints the title of a Star Wars movie where the episode number matches a given integer.
+const req = require('request');
+const url = process.argv[2];
+
+req(url, (e, response, data) => {
+  if (e) {
+    console.error(e);
+  } else {
+    const movies = JSON.parse(data).results;
+    const characterCount = movies.filter(movie => movie.characters.includes('https://swapi-api.hbtn.io/api/people/18/'));
+    console.log(characterCount.length);
+  }
+});
