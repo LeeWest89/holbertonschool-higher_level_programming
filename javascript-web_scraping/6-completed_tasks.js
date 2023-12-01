743d17d3ebf088d39949488921610fb9d4c3c880
LeeWest89 <6683@holbertonstudents.com>
2023-12-01 11:41:54 -0800

    6 is testing well, think it ready for the checker, also created an exit on a error.

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,22 @@
-#include
+#!/usr/bin/node
+// computes the number of tasks completed by user id.
+const req = require('request');
+const url = process.argv[2];
+
+req(url, (e, response, data) => {
+  if (e) {
+    process.exit(1);
+  }
+  const todos = JSON.parse(data);
+  const finishedTodos = {};
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (finishedTodos[todo.userId]) {
+        finishedTodos[todo.userId]++;
+      } else {
+        finishedTodos[todo.userId] = 1;
+      }
+    }
+  });
+  console.log(finishedTodos);
+});
