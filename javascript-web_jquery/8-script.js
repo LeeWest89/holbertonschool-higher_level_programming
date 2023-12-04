e57f157e7e57e4e22da8f04734a81b831f0ec382
LeeWest89 <6683@holbertonstudents.com>
2023-12-03 16:56:35 -0800

    found an example of how to append  a list testing it to see if it works

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,1 +1,4 @@
 #!/usr/bin/node
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data){
+  $('UL#list_movies').append(...data.results.map(film => `<li>${film.title}</li>`));
+});
