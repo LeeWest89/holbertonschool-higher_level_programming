6b04dc09d7eb01233c6179bfcbfcfd88ad31962b
LeeWest89 <6683@holbertonstudents.com>
2023-12-04 07:42:00 -0800

    had a semistandard error on modified task 8 line 4 space before key

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // fetches and lists the title for all movies by using the URL
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
-  $.each(data.results, function ( key, film) {
+  $.each(data.results, function (key, film) {
     $('UL#list_movies').append(`<li>${film.title}</li>`);
   });
 });
