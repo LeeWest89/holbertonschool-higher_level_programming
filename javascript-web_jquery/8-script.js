5353043cc5b67b209c1af68b8e9e80f42ad1a798
LeeWest89 <6683@holbertonstudents.com>
2023-12-03 22:21:50 -0600

    found a more readable way to do 8

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 // fetches and lists the title for all movies by using the URL
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
-  $('UL#list_movies').append(...data.results.map(film => `<li>${film.title}</li>`));
+  $.each(data.results, function ( key, film) {
+    $('UL#list_movies').append(`<li>${film.title}</li>`);
+  });
 });
